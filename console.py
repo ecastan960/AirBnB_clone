3e48218c7832e8b8c50993bf22af62d267875927
ecastan960 <ecastan1@eafit.edu.co>
2021-02-15 15:57:43 -0500

    04|Update to console

--- console.py
+++ console.py
@@ -60,7 +60,7 @@ class HBNBCommand(cmd.Cmd):
             # print(y[0][0])
             # print (inspect.getmembers(sys.modules[__name__], inspect.isclass))
             #clases = [command[0] for command in y]
-        
+
             # print(clases)
             if args[0] in self.clases:
                 x = eval(str(args[0])+'()')
@@ -88,6 +88,29 @@ class HBNBCommand(cmd.Cmd):
             print("** no instance found **")
         else:
             print(objs[name])
+
+    def do_destroy(self, *args):
+        objs = storage.all()
+        tokens = args[0].split()
+
+        if len(tokens) > 1:
+            name = str(tokens[0]) + "." + str(tokens[1])
+
+        if len(tokens) == 0:
+            print("** class name missing **")
+        elif tokens[0] not in self.clases:
+            print("** class doesn't exist **")
+        elif len(tokens) == 1:
+            print("** instance id missing **")
+        elif name not in objs:
+            print("** no instance found **")
+        else:
+            print(objs)
+            print("---")
+            objs.pop(name)
+            print(objs)
+            storage.save()
+            
 
 if __name__ == '__main__':
 
