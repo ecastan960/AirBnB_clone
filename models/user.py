3ab1c937d6543a86047d08294538aad85395fe34
ecastan960 <ecastan1@eafit.edu.co>
2021-02-16 19:54:31 -0500

    10|Comments and pep8 fixes

--- models/user.py
+++ models/user.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
-
+"""[summary]
+"""
 import models
 
+
 class User(models.base_model.BaseModel):
+    """[summary]
+
+    Args:
+        models ([type]): [description]
+    """
     email = ""
     password = ""
     first_name = ""
