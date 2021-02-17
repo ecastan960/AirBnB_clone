b5fd8d943d818e46fe3b823edb95c7279d9faa26
Lucho <2450@holbertonschool.com>
2021-02-17 11:14:33 -0500

    Kwargs passed as unpacked dict

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -25,13 +25,37 @@ class Test_basics(unittest.TestCase):
         self.assertFalse(model2.id == model3.id)
 
     def test_ignore_args(self):
-        """ Tests that default attr are set even with args """
+        """Testing if args are ignored"""
 
         my_model = BaseModel("test", 18)
-        
+
         self.assertTrue(hasattr(my_model, "id"))
         self.assertTrue(hasattr(my_model, "created_at"))
         self.assertTrue(hasattr(my_model, "updated_at"))
+
+    def test_kwargs(self):
+        """Unpacked dictionary as argument"""
+
+        my_dict = {'id': '99h15869-ki5k-5r9p-f0y1-t168f8b27636',
+             'created_at': '2020-02-16T11:03:58.053312',
+             '__class__': 'BaseModel',
+             'updated_at': '2020-02-16T11:06:59.053312'}
+
+        my_model = BaseModel(**my_dict)
+
+        self.assertEqual(my_model.id, '99h15869-ki5k-5r9p-f0y1-t168f8b27636')
+        self.assertEqual(my_model.created_at.isoformat(),
+                         '2020-02-16T11:03:58.053312')
+        self.assertEqual(my_model.updated_at.isoformat(),
+                         '2020-02-16T11:06:59.053312')
+
+        self.assertTrue(hasattr(my_model, "id"))
+        self.assertTrue(hasattr(my_model, "created_at"))
+        self.assertTrue(hasattr(my_model, "updated_at"))
+        self.assertTrue(hasattr(my_model, "__class__"))
+        self.assertTrue(my_model.__class__ not in my_model.__dict__)
+
+        
 
 if __name__ == "__main__":
     unittest.main()
