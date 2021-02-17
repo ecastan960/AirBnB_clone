573e80e4610a2388d4c1339cc466323021316eb5
Lucho <2450@holbertonschool.com>
2021-02-17 11:17:25 -0500

    __class__ key is ignored

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -38,7 +38,6 @@ class Test_basics(unittest.TestCase):
 
         my_dict = {'id': '99h15869-ki5k-5r9p-f0y1-t168f8b27636',
              'created_at': '2020-02-16T11:03:58.053312',
-             '__class__': 'BaseModel',
              'updated_at': '2020-02-16T11:06:59.053312'}
 
         my_model = BaseModel(**my_dict)
@@ -55,6 +54,27 @@ class Test_basics(unittest.TestCase):
         self.assertTrue(hasattr(my_model, "__class__"))
         self.assertTrue(my_model.__class__ not in my_model.__dict__)
 
+    def test_kwargs_with_class(self):
+        """Unpacked dictionary has 'class' key and is ignored"""
+
+        my_dict = {'id': '99h15869-ki5k-5r9p-f0y1-t168f8b27636',
+             'created_at': '2020-02-16T11:03:58.053312',
+             'updated_at': '2020-02-16T11:06:59.053312',
+             '__class__': 'BaseModel'}
+
+        my_model = BaseModel(**my_dict)
+
+        self.assertEqual(my_model.id, '99h15869-ki5k-5r9p-f0y1-t168f8b27636')
+        self.assertEqual(my_model.created_at.isoformat(),
+                         '2020-02-16T11:03:58.053312')
+        self.assertEqual(my_model.updated_at.isoformat(),
+                         '2020-02-16T11:06:59.053312')
+
+        self.assertTrue(hasattr(my_model, "id"))
+        self.assertTrue(hasattr(my_model, "created_at"))
+        self.assertTrue(hasattr(my_model, "updated_at"))
+        self.assertTrue(hasattr(my_model, "__class__"))
+        self.assertTrue(my_model.__class__ not in my_model.__dict__)
         
 
 if __name__ == "__main__":
