079ffb90a18967b6412cbcae3336a955fe684b40
Lucho <2450@holbertonschool.com>
2021-02-17 10:59:09 -0500

    Test for id checking added

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -14,6 +14,16 @@ class Test_basics(unittest.TestCase):
         self.assertTrue(hasattr(my_model, "created_at"))
         self.assertTrue(hasattr(my_model, "updated_at"))
 
-    
+    def test_id_diff_for_each_instance(self):
+        """Each id must be diferent"""
+        model1 = BaseModel()
+        model2 = BaseModel()
+        model3 = BaseModel()
+
+        self.assertFalse(model1.id == model2.id)
+        self.assertFalse(model1.id == model3.id)
+        self.assertFalse(model2.id == model3.id)
+
+
 if __name__ == "__main__":
     unittest.main()
