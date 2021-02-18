feabc9bcafd1a843cf5e876517d693428189592b
Lucho <2450@holbertonschool.com>
2021-02-17 20:45:29 -0500

    Console tests file created

---
+++ tests/test_console.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Console tests"""
+
+import unittest
+
+
+class TestConsole(unittest.TestCase):
+    """Tests for console Module"""
+
+    def test_idk(self):
+        """This test is a test"""
+
+        pass
