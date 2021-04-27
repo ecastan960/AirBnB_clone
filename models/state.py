d6d6bf9bbb7feac976041d980d92041383577e50
ecastan960 <ecastan1@eafit.edu.co>
2021-04-27 11:22:30 -0500

    13|Update comments

--- models/state.py
+++ models/state.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-"""[summary]
+"""This file creates a class
+that inherits from BaseModel
 """
 import models
 
 
 class State(models.base_model.BaseModel):
-    """[summary]
-
-    Args:
-        models ([type]): [description]
+    """State class
     """
     name = ""
