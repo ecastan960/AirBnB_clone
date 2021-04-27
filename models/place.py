3b957c6b6aeb6510fa0ef85cf8ca89945d64e1d0
ecastan960 <ecastan1@eafit.edu.co>
2021-04-27 11:21:37 -0500

    13|Update comments

--- models/place.py
+++ models/place.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-"""[summary]
+"""This file creates a class
+that inherits from BaseModel
 """
 import models
 
 
 class Place(models.base_model.BaseModel):
-    """[summary]
-
-    Args:
-        models ([type]): [description]
+    """Place class
     """
     city_id = ""
     user_id = ""
